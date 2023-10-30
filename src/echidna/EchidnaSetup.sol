1ad9fff498f450e3af5b8597f7fb15fb84690fa4
Rappie <jasper@rappie.com>
2023-10-30 12:04:20 +0100

    Debugger import

--- src/echidna/EchidnaSetup.sol
+++ src/echidna/EchidnaSetup.sol
@@ -2,6 +2,7 @@
 
 import "./IHevm.sol";
 import "./EchidnaConfig.sol";
+import "./Debugger.sol";
 
 contract EchidnaSetup is EchidnaConfig {
     IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
