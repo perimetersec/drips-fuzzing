fc75464f2d63ff8f682a0a232901733f9f5dab97
Rappie <jasper@rappie.com>
2023-11-14 15:28:34 +0100

    No helpers needed in Echidna.sol

--- src/echidna/Echidna.sol
+++ src/echidna/Echidna.sol
@@ -3,20 +3,14 @@
 import "./base/EchidnaBase.sol";
 import "./EchidnaTestInvariant.sol";
 import "./EchidnaTest.sol";
-import "./EchidnaSplitsHelpers.sol";
 import "./EchidnaSplitsTests.sol";
-import "./EchidnaStreamsHelpers.sol";
 import "./EchidnaStreamsTests.sol";
-import "./EchidnaSqueezeHelpers.sol";
 import "./EchidnaSqueezeTests.sol";
 
 contract Echidna is
     EchidnaTestInvariant,
     EchidnaTest,
-    // EchidnaSplitsHelpers,
     EchidnaSplitsTests,
-    // EchidnaStreamsHelpers,
     EchidnaStreamsTests,
-    // EchidnaSqueezeHelpers,
     EchidnaSqueezeTests
 {}
