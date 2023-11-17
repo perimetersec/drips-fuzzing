69c25bd3558e1cd790af69686b82bdad33601c00
Rappie <jasper@rappie.com>
2023-11-17 21:53:13 +0100

    Documentation

--- src/echidna/base/EchidnaBase.sol
+++ src/echidna/base/EchidnaBase.sol
@@ -4,6 +4,10 @@ import "./EchidnaSetup.sol";
 import "./EchidnaStorage.sol";
 import "./EchidnaAccounting.sol";
 
+/**
+ * @title Mixin grouping together all base contracts
+ * @author Rappie
+ */
 contract EchidnaBase is
     EchidnaSetup,
     EchidnaStorage,
