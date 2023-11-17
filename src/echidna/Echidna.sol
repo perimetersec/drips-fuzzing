8d9f59e751ac24eab894734af38cbfe64b81b57c
Rappie <jasper@rappie.com>
2023-11-17 21:51:55 +0100

    Documentation

--- src/echidna/Echidna.sol
+++ src/echidna/Echidna.sol
@@ -6,10 +6,31 @@ import "./EchidnaSplitsTests.sol";
 import "./EchidnaStreamsTests.sol";
 import "./EchidnaSqueezeTests.sol";
 
+/**
+ * @title Echidna contract for testing the Drips contract
+ * @author Rappie
+ * @dev Running the tests:
+ * For Echidna use `echidna . --contract Echidna --config echidna-config.yaml`.
+ * For Medusa use `medusa fuzz`.
+ * The tests are split into multiple files to be able to toggle the fuzzing
+ * of specific features on and off. This is done by commenting out the
+ * corresponding inherited contract in the Echidna contract below.
+ * Basic tests contain basic features like giving, receiving, splitting and
+ * collecting.
+ * Splits tests contain tests for splitting.
+ * Streams tests contain tests for receiving streams.
+ * Squeeze tests contain tests for squeezing.
+ * Invariant tests contain tests for invariant properties of the system. These
+ * make most sense with all other tests enabled.
+ * For further performance improvements, resource heavy tests can be toggled.
+ * To do so, change the value of TOGGLE_HEAVY_TESTS_ENABLED in EchidnaConfig.
+ */
 contract Echidna is
-    EchidnaInvariantTests,
     EchidnaBasicTests,
     EchidnaSplitsTests,
     EchidnaStreamsTests,
-    EchidnaSqueezeTests
-{}
+    EchidnaSqueezeTests,
+    EchidnaInvariantTests
+{
+
+}
